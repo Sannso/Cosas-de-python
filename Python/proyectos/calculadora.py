78eec51eec595111b5bdd53f686d7b1a56793519
Santiago <santi-2000-12@hotmail.com>
2019-07-21 19:59:25 -0500

    Actualizacion Calculadora
    Acabe de hacer que se muestren los numero que se oprimen en la calculadora, y ademas agregarlos a una variable para ser ultilizada luego en las operaciones

--- Python/proyectos/calculadora.py
+++ Python/proyectos/calculadora.py
@@ -19,8 +19,27 @@ from tkinter import *
 root = Tk()
 root.title("Calculadora")
 root.iconbitmap("gato.ico")
-root.resizable(1,1)
+root.resizable(0,0)
 root.config(bg="black")
+
+
+#------------ Primer metodo -------------------
+
+def primerNumero(numero):
+    if(numero >= 0 or numero <= 9):
+        varR.set(varR.get()+str(numero))
+
+
+
+
+def intermediario():
+    primerNumero(7)
+
+
+
+
+
+
 
 
 framePantalla = Frame(root, width="320", height="480")
@@ -33,9 +52,10 @@ mFrame.pack(side="bottom")
 
 
 # "Pantalla"
-resultado = Label(framePantalla, text="Aqui va el resultado")
+varR = StringVar()
+resultado = Label(framePantalla, text="0", textvariable=varR)
 #resultado.grid(row="0", column="0", padx="10", pady="10")
-resultado.pack(pady="8")
+resultado.pack(pady="8", padx="8", side="right")
 
 
 
@@ -57,14 +77,18 @@ Dividir.grid(row="1", column="3", padx="4", pady="6" )
 
 
 
+siete = Button(mFrame, text="7", justify="center", width="7", height="2",
+                    bg="black", fg="white", command=lambda: primerNumero(7))
 # Botones "7", "8", "9", "X"
-siete = Button(mFrame, text="7", justify="center", width="7", height="2")
 siete.grid(row="2", column="0", padx="4" )
 
-ocho = Button(mFrame, text="8", justify="center", width="7", height="2")
+
+ocho = Button(mFrame, text="8", justify="center", width="7", height="2",
+                    bg="black", fg="white", command=lambda: primerNumero(8))
 ocho.grid(row="2", column="1", padx="4")
 
-nueve = Button(mFrame, text="9", justify="center", width="7", height="2")
+nueve = Button(mFrame, text="9", justify="center", width="7", height="2",
+                    bg="black", fg="white", command=lambda: primerNumero(9))
 nueve.grid(row="2", column="2", padx="4" )
 
 Multip = Button(mFrame, text="x", justify="center", width="7", height="2")
@@ -75,13 +99,16 @@ Multip.grid(row="2", column="3", padx="4")
 
 
 # Botones "4", "5", "6", "-"
-cuatro = Button(mFrame, text="4", justify="center", width="7", height="2")
+cuatro = Button(mFrame, text="4", justify="center", width="7", height="2",
+                    bg="black", fg="white", command=lambda: primerNumero(4))
 cuatro.grid(row="3", column="0", padx="4", pady="6" )
 
-cinco = Button(mFrame, text="5", justify="center", width="7", height="2")
+cinco = Button(mFrame, text="5", justify="center", width="7", height="2",
+                    bg="black", fg="white", command=lambda: primerNumero(5))
 cinco.grid(row="3", column="1", padx="4", pady="6")
 
-seis = Button(mFrame, text="6", justify="center", width="7", height="2")
+seis = Button(mFrame, text="6", justify="center", width="7", height="2",
+                    bg="black", fg="white", command=lambda: primerNumero(6))
 seis.grid(row="3", column="2", padx="4", pady="6" )
 
 Restar = Button(mFrame, text="-", justify="center", width="7", height="2")
@@ -91,13 +118,16 @@ Restar.grid(row="3", column="3", padx="4", pady="6")
 
 
 # Botones "1", "2", "3", "+"
-uno = Button(mFrame, text="1", justify="center", width="7", height="2")
+uno = Button(mFrame, text="1", justify="center", width="7", height="2",
+                    bg="black", fg="white", command=lambda: primerNumero(1))
 uno.grid(row="4", column="0", padx="4")
 
-dos = Button(mFrame, text="2", justify="center", width="7", height="2")
+dos = Button(mFrame, text="2", justify="center", width="7", height="2",
+                    bg="black", fg="white", command=lambda: primerNumero(2))
 dos.grid(row="4", column="1", padx="4")
 
-tres = Button(mFrame, text="3", justify="center", width="7", height="2")
+tres = Button(mFrame, text="3", justify="center", width="7", height="2",
+                    bg="black", fg="white", command=lambda: primerNumero(3))
 tres.grid(row="4", column="2", padx="4")
 
 Sumar = Button(mFrame, text="+", justify="center", width="7", height="2")
@@ -110,7 +140,8 @@ Sumar.grid(row="4", column="3", padx="4")
 neg = Button(mFrame, text="neg", justify="center", width="7", height="2")
 neg.grid(row="5", column="0", padx="4", pady="6" )
 
-cero = Button(mFrame, text="0", justify="center", width="7", height="2")
+cero = Button(mFrame, text="0", justify="center", width="7", height="2",
+                    bg="black", fg="white", command=lambda: primerNumero(0))
 cero.grid(row="5", column="1", padx="4", pady="6")
 
 coma = Button(mFrame, text=",", justify="center", width="7", height="2")
